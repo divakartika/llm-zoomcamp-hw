3b131e2741bb250e3a9f02c44dc157bf769b1fbe
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2026-06-21 06:12:49 +0000

    answers Q1-Q5

---
+++ ingest.py
@@ -0,0 +1,47 @@
+import time
+from minsearch import Index
+from sqlitesearch import TextSearchIndex
+from gitsource import GithubRepositoryDataReader
+
+def load_github_data():
+
+    reader = GithubRepositoryDataReader(
+        repo_owner="DataTalksClub",
+        repo_name="llm-zoomcamp",
+        commit_id="8c1834d",
+        allowed_extensions={"md"},
+        filename_filter=lambda path: "/lessons/" in path,
+    )
+
+    files = reader.read()
+    documents = []
+
+    for file in files:
+        doc = file.parse()
+        documents.append(doc)
+    
+    return documents
+
+def build_index(documents):
+    index = Index(
+        text_fields=["content"],
+        keyword_fields=["filename"]
+    )
+
+    index.fit(documents)
+    return index
+
+def build_persistent_index(documents):
+    index = TextSearchIndex(
+        text_fields=["content"],
+        keyword_fields=["filename"],
+        db_path="github.db"
+    )
+
+    for doc in documents:
+        index.add(doc)
+        print(f"""Added: {doc["filename"][:60]}...""")
+        time.sleep(0.5)
+
+    print("Done. Index saved to github.db")
+    return index
---
+++ rag_helper.py
@@ -0,0 +1,83 @@
+INSTRUCTIONS = """
+Your task is to answer questions from the course participants
+based on the provided context.
+
+Use the context to find relevant information and provide accurate
+answers. If the answer is not found in the context,
+respond with "I don't know."
+"""
+
+PROMPT_TEMPLATE = """
+QUESTION: {question}
+
+CONTEXT:
+{context}
+""".strip()
+
+class RAGBase:
+    def __init__(
+        self,
+        index,
+        llm_client,
+        instructions=INSTRUCTIONS,
+        prompt_template=PROMPT_TEMPLATE,
+        # course="llm-zoomcamp",
+        model="gpt-5.4-mini"
+    ):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        # self.course = course
+        self.prompt_template = prompt_template
+        self.model = model
+
+    def search(self, question, num_results=5):
+        # boost_dict = {"question": 2.0, "section": 0.5}
+        # filter_dict = {"course": self.course}
+
+        return self.index.search(
+            question,
+            # boost_dict=boost_dict,
+            # filter_dict=filter_dict,
+            num_results=num_results
+        )
+
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append(doc["filename"])
+            # lines.append("Q: " + doc["question"])
+            lines.append("Content: " + doc["content"])
+            lines.append("")
+
+        return "\n".join(lines).strip()
+
+    def build_prompt(self, question, search_results):
+        context = self.build_context(search_results)
+        prompt = self.prompt_template.format(
+            question=question,
+            context=context
+        )
+        return prompt #.strip()
+
+    def llm(self, user_prompt):
+        message_history = [
+            {"role": "developer", "content": self.instructions},
+            {"role": "user", "content": user_prompt}
+        ]
+
+        response = self.llm_client.responses.create(
+            model=self.model,
+            input=message_history
+        )
+
+        return response # .output_text
+
+    # Combining them all into one function: rag()
+
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        answer = self.llm(prompt)
+        return answer
