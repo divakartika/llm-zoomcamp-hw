e8704b5b6acc7b096f32119e8e6dc4cf36b29fe6
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2026-06-29 16:26:53 +0000

    hw 2 Q1-5

--- ingest.py
+++ ingest.py
@@ -1,7 +1,27 @@
 import time
+import requests
 from minsearch import Index
 from sqlitesearch import TextSearchIndex
 from gitsource import GithubRepositoryDataReader
+
+def load_faq_data():
+    docs_url = "https://datatalks.club/faq/json/courses.json"
+    response = requests.get(docs_url)
+    courses_raw = response.json()
+
+    documents = []
+    url_prefix = "https://datatalks.club/faq"
+
+    for course in courses_raw:
+        course_url = f"""{url_prefix}{course["path"]}"""
+
+        course_response = requests.get(course_url)
+        course_response.raise_for_status()
+        course_data = course_response.json()
+
+        documents.extend(course_data)
+
+    return documents
 
 def load_github_data():
 
